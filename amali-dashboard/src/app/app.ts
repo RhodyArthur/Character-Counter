06919a668332c36f79e9ad89d2ab808f0b8947c4
RhodyArthur <rhodaarthur48@gmail.com>
2025-09-10 09:43:20 +0000

    chore: integrate at-design-system styles and custom elements

--- amali-dashboard/src/app/app.ts
+++ amali-dashboard/src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core'
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class App {
   protected title = 'amali-dashboard';
